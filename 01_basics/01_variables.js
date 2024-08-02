6b92ac6f9f7d59bc95c422089e6ab143b761a8f0
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-02 08:20:55 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 687545;
+let accountEmail = "ajitinfotech123@google.com";
+var accountPassword = "123547";
+let accountState;
+
+accountCity = "Gurgaon"; // This is also possible to make a variable. without using keywords let, const, var.
+
+console.log(accountId);
+
+/*
+The keywords var, let, and const in JavaScript define the variable scope and behavior. 
+The var keyword has function scope and is hoisted. 
+The let and const keywords have block scope.
+Perfer not to use var.
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
