fe38f8f0c9b6f03d791064083257e7dbb0a45e70
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-03 08:12:55 +0000

    Conversion

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "33abc";
+
+console.log(typeof (score));
+
+let valueInNUmber = Number(score);
+console.log(typeof valueInNUmber);
+console.log(valueInNUmber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0 ;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggiedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggiedIn);
+
+/*
+1 => true; 
+0 => false;
+"Ajit" => true;
+*/ 
+
+let someNumber = 35;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+
+
+
