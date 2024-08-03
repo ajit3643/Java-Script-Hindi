a03207fef259658b9487fc8e8b5b242ec1190339
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-03 06:46:31 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+// Datatypes in javascript!
+"use strict"; // treat all JS code as newer version!
+// alert(3 + 3)  // we are using nodejs, not browser!
+
+console.log(3+ 3);
+
+// code readability should be high.
+
+let age = 18;
+let isAdult = true;
+let name = "Ajit";
+let address; // undefined
+let state = null;
+
+
+
+/*
+There are two types of datatypes!
+
+Primitive
+. number 
+. bigint
+. string   => ""
+. boolen   => true/false
+. null
+. undefined 
+. symbol
+
+Non - primitive
+. Array
+. object 
+. RegExp
+
+*/
+
+
+
