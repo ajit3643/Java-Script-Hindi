2994a145acf2e8066aa43a150f87bfc847727673
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-09 16:00:12 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,32 @@
+// Basic comparison!
+//  console.log(2 > 1);
+//  console.log(2 >= 1);
+//  console.log(2 < 1);
+//  console.log(2 <= 1);
+//  console.log(2 == 1);
+//  console.log(2 != 1);
+ 
+console.log("2" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null === 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+
+
+// === that's mean strict check!
+
+console.log("2" === 1);
+
+
+
+
+
+
+
+
