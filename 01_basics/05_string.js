b876087fa9a0a71f13c37f50f9d3c79a168060d7
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-10 13:24:53 +0000

    String_methods

---
+++ 01_basics/05_string.js
@@ -0,0 +1,21 @@
+const name =  "Ajit";
+const repoCount = 10;
+
+console.log(name + repoCount);
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+console.log(name.length);
+console.log(name.charAt(0));
+console.log(name.indexOf('t'));
+
+console.log(name.toUpperCase());
+let newName = name.concat( " Kumar");
+console.log(newName.length);
+console.log(newName);
+
+
+
+
+
+
+
