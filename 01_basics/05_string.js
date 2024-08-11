b3c610472e7dfbfe8a8a00c8c5abee8adad98a1a
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-11 05:42:40 +0000

    String methods

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,3 +1,11 @@
+// Creating strings
+// Strings can be created as primitives, from string literals, or as objects, using the String() constructor:
+
+const string1 = "A string primitive";
+const string2 = 'Also a string primitive';
+const string3 = `Yet another string primitive`;
+const string4 = new String("A String object");
+
 const name =  "Ajit";
 const repoCount = 10;
 
