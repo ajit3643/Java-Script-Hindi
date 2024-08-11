138a3d38fe47fbc0372bc13153925ddfbf172cba
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-11 06:24:54 +0000

    String methods

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -21,6 +21,42 @@ let newName = name.concat( " Kumar");
 console.log(newName.length);
 console.log(newName);
 
+// String Length
+// To find the length of a string, use the built-in length property:
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+/*
+Extracting String Characters
+There are 4 methods for extracting string characters:
+
+The at(position) Method
+The charAt(position) Method
+The charCodeAt(position) Method
+Using property access [] like in arrays
+*/ 
+// JavaScript String charAt()
+// The charAt() method returns the character at a specified index (position) in a string:
+
+let char = text.charAt(5);
+console.log(char);
+
+/*
+Extracting String Parts
+There are 3 methods for extracting a part of a string:
+
+slice(start, end)
+substring(start, end)
+substr(start, length)
+JavaScript String slice()
+slice() extracts a part of a string and returns the extracted part in a new string.
+
+The method takes 2 parameters: start position, and end position (end not included).
+*/ 
+
+let part = text.slice(7, 13);
+
+
 
 
 
