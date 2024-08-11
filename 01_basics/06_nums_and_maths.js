f4af6f4e6157591110786b22929c2fea4223ad43
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-11 11:12:31 +0000

    Numbers & Maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,23 @@
+/* 
+let score = 100;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8966;
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000;
+console.log(hundreds.toLocaleString('en-IN'));
+*/
+
+// ************** Maths **************************
+
+console.log(Math);
+
+
+
+
