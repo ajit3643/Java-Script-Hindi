8e73e0f58a8bacd3f2cdc42a9f95785781d0f268
Ajit Kumar <115556587+ajit3643@users.noreply.github.com>
2024-08-22 09:48:24 +0000

    Updated

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -1,9 +1,9 @@
 // Array in JS.
 
-const arr = [0,1,2,3,4,5,true,"Ajit"];
+const arr = [0, 1, 2, 3, 4, 5, true, "Ajit"];
 //console.log(arr[4]);
 
-const newArr = new Array(1,2,3,4,5);
+const newArr = new Array(1, 2, 3, 4, 5);
 //console.log(newArr);
 
 
@@ -17,7 +17,7 @@ const newArr = new Array(1,2,3,4,5);
 
 // push () - It adds elements to the end of the array.
 
-let myArr = [1,2,3,4,5];
+let myArr = [1, 2, 3, 4, 5];
 //console.log(myArr);
 
 // Before apply push () method. Output is :- [ 1, 2, 3, 4, 5 ]
@@ -56,7 +56,7 @@ let newArray = myArr.join();
 // console.log(newArray);
 
 // sort () :- It sorts the elements of an array.
-newArray = [64,45,3,92,44,64];
+newArray = [64, 45, 3, 92, 44, 64];
 // console.log(newArray);
 // console.log(newArray.sort());
 
@@ -64,15 +64,26 @@ newArray = [64,45,3,92,44,64];
 
 // slice, splice
 
-console.log("A",myArr);
+console.log("A", myArr);
 
-let myn1 = myArr.slice(1,3);
+let myn1 = myArr.slice(1, 3);
 console.log(myn1);
 console.log("B", myArr);
-
-let myn2 = myArr.splice(1,3);
+/*
+The slice() method of Array instances returns a shallow copy of a portion of an array into a new array 
+object selected from start to end (end not included) where start and end represent the index of items in that array. 
+The original array will not be modified.
+*/
+let myn2 = myArr.splice(1, 3);
 console.log(myn2);
-console.log("C",myArr);
+console.log("C", myArr);
+
+/*
+The splice() method of Array instances changes the contents of an array 
+by removing or replacing existing elements and/or adding new elements in place.
+To create a new array with a segment removed and/or replaced without mutating the original array, 
+use toSpliced(). To access part of an array without modifying it, see slice().
+*/
 
 
 
